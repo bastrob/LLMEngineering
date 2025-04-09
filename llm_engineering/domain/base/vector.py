095b13451f06ea4e6678beaf6ace1bf76879008b
bastrob <bastien.robert91@gmail.com>
2025-04-10 00:01:55 +0200

    OVM files

---
+++ llm_engineering/domain/base/vector.py
@@ -0,0 +1,147 @@
+import uuid
+from abc import ABC
+from typing import Generic, Type, TypeVar
+from uuid import UUID
+
+import numpy as np
+from loguru import logger
+from pydantic import UUID4, BaseModel, Field
+from qdrant_client.http import exceptions
+from qdrant_client.http.models import Distance, VectorParams
+from qdrant_client.models import CollectionInfo, PointStruct, Record
+
+from llm_engineering.domain.exceptions import ImproperlyConfigured
+from llm_engineering.infrastructure.db.qdrant import connection
+
+T = TypeVar("T", bound="VectorBaseDocument")
+
+
+class VectorBaseDocument(BaseModel, Generic[T], ABC):
+    id: UUID4 = Field(default_factory=uuid.uuid4)
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, self.__class__):
+            return False
+        
+        return self.id == value.id
+    
+    def __hash__(self) -> int:
+        return hash(self.id)
+    
+    @classmethod
+    def from_record(cls: Type[T], point: Record) -> T:
+        _id = UUID(point.id, version=4)
+        payload = point.payload or {}
+
+        attributes = {
+            "id": _id,
+            **payload,
+        }
+        if cls._has_class_attributes("embedding"):
+            payload["embedding"] = point.vector or None
+        
+        return cls(**attributes)
+    
+    def to_point(self: T, **kwargs) -> PointStruct:
+        exclude_unset = kwargs.pop("exclude_unset", False)
+        by_alias = kwargs.pop("by_alias", True)
+
+        payload = self.model_dump(exclude_unset=exclude_unset, by_alias=by_alias, **kwargs)
+
+        _id = str(payload.pop("id"))
+        vector = payload.pop("embedding", {})
+        if vector and isinstance(vector, np.ndarray):
+            vector = vector.tolist()
+        
+        return PointStruct(id=_id, vector=vector, payload=payload)
+
+    @classmethod
+    def bulk_insert(cls: Type[T], documents: list["VectorBaseDocument"]) -> bool:
+        try:
+            cls._bulk_insert(documents)
+        except exceptions.UnexpectedResponse:
+            logger.info(
+                f"Collection '{cls.get_collection_name()}' does not exist. Trying to create the collection and reinsert the documents.")
+
+            cls.create_collection()
+
+            try:
+                cls._bulk_inster(documents)
+            except exceptions.UnexpectedResponse:
+                logger.error(f"Failed to insert documents in '{cls.get_collection_name()}'.")
+
+                return False
+        
+        return True
+
+    @classmethod
+    def _bulk_insert(cls: Type[T], documents: list["VectorBaseDocument"]) -> None:
+        points = [doc.to_point() for doc in documents]
+
+        connection.upsert(collection_name=cls.get_collection_name(), points=points)
+    
+    @classmethod
+    def bulk_find(cls: Type[T], limit: int = 10, **kwargs) -> tuple[list[T], UUID | None]:
+        try:
+            documents, next_offset = cls._bulk_find(limit=limit, **kwargs)
+        except exceptions.UnexpectedResponse:
+            logger.error(f"Failed to search documents in '{cls.get_collection_name}'.")
+
+            documents, next_offset = [], None
+        
+        return documents, next_offset
+
+    @classmethod
+    def _bulk_find(cls: Type[T], limit: int = 10, **kwargs) -> tuple[list[T], UUID | None]:
+        collection_name = cls.get_collection_name()
+
+        offset = kwargs.pop("offset", None)
+        offset = str(offset) if offset else None
+
+        records, next_offset = connection.scroll(
+            collection_name=collection_name,
+            limit=limit,
+            with_payload=kwargs.pop("with_payload", True),
+            with_vectors=kwargs.pop("with_vectors", False),
+            offset=offset,
+            **kwargs,
+        )
+        documents = [cls.from_record(record) for record in records]
+        if next_offset is not None:
+            next_offset = UUID(next_offset, version=4)
+        
+        return documents, next_offset
+
+    @classmethod
+    def search(cls: Type[T], query_vector: list, limit: int = 10, **kwargs) -> list[T]:
+        try:
+            documents = cls._search(query_vector=query_vector, limit=limit, **kwargs)
+        except exceptions.UnexpectedResponse:
+            logger.error(f"Failed to search documents in '{cls.get_collection_name()}'.")
+
+            documents = []
+        return documents
+    
+    @classmethod
+    def _search(cls: Type[T], query_vector: list, limit: int = 10, **kwargs) -> list[T]:
+        collection_name = cls.get_collection_name()
+        records = connection.search(
+            collection_name=collection_name,
+            query_vector=query_vector,
+            limit=limit,
+            with_payload=kwargs.pop("with_payload", True),
+            with_vectors=kwargs.pop("with_vectors", False),
+            **kwargs,
+        )
+        documents = [cls.from_record(record) for record in records]
+
+        return documents
+    
+    @classmethod
+    def get_collection_name(cls: Type[T]) -> str:
+        if not hasattr(cls, "Config") or not hasattr(cls.Config, "name"):
+            raise ImproperlyConfigured(
+                "The class should define a Config class with" "the 'name' property that reflects the collection's name."
+            )
+        return cls.Config.name
+    
